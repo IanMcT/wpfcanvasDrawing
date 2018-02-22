d20c9298509c589d15428e821b77418f7ddbbe84
IanMcT <ian.mctavish@tldsb.on.ca>
2018-02-22 06:10:07 -0500

    Add files via upload

---
+++ canvasImage/canvasImage/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace canvasImage
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        double t = 20;
+        double l = 20;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGetFile_Click(object sender, RoutedEventArgs e)
+        {
+            if (canvas.Children.Count > 0)
+            {
+                canvas.Children.RemoveAt(0);
+            }
+            Microsoft.Win32.OpenFileDialog openFileD = new Microsoft.Win32.OpenFileDialog();
+            openFileD.ShowDialog();
+
+            BitmapImage bi = new BitmapImage(new Uri(openFileD.FileName));
+            System.Windows.Media.ImageBrush ib = new ImageBrush(bi);
+            canvas.Background = ib;
+
+            Rectangle r = new Rectangle();
+            r.Stroke = System.Windows.Media.Brushes.GreenYellow;
+            r.Width = 80;
+            r.Height = 60;
+            r.StrokeThickness = 2;
+            
+            canvas.Children.Add(r);
+            Canvas.SetLeft(r, l);
+            Canvas.SetTop(r, t);
+            l += 20;
+            t += 20;
+            
+        }
+    }
+}
